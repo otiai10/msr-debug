ee53fe8b52686b41807568d85d2ac9ec886931b7
otiai10 <otiai10@gmail.com>
2017-02-17 11:15:47 +0100

    setup to demo

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const main = () => {
   console.log('MediaStreamRecorder:', typeof MediaStreamRecorder);
   navigator.getUserMedia({audio:false, video:true}, (stream) => {
 
+    // {{{ Just to display what to capture
+    document.querySelector('video#video').src = URL.createObjectURL(stream);
+    // }}}
+
     var recorder = new MediaStreamRecorder(stream);
     // recorder.mimeType = 'video/mp4';
     recorder.mimeType = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
@@ -10,15 +14,21 @@ const main = () => {
 
     recorder.ondataavailable = (blob) => {
       const url = URL.createObjectURL(blob);
-      // It works, looks good.
-      window.open(url);
 
-      // When I downloaded it as a file,
-      var a = document.createElement('a');
-      a.href = url;
-      a.download = 'msr-example.mp4';
-      a.click();
-      // successful, but not playable on QuickTime Player
+      if (window.confirm('Open recorded mp4 in your browser?')) {
+        // It works, looks good.
+        window.open(url);
+      }
+
+      if (window.confirm('Download recorded mp4 on your local?')) {
+        // When I downloaded it as a file,
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = 'msr-example.mp4';
+        a.click();
+        // successful, but not playable on QuickTime Player
+      }
+
     };
 
     // Record it for 3s
